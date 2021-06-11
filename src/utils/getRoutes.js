7aa2f08d06f7920e2d3aca076cd6c8d6ca7c0a47
IlKhr <khorishko.ilya@gmail.com>
2021-06-11 11:29:34 +0300

    feat(data): add all data for pages

--- src/utils/getRoutes.js
+++ src/utils/getRoutes.js
@@ -1,7 +1,6 @@
 const getRoutes = (staticData, pageData) => {
   return staticData.header.reduce((acc, item) => {
     for (const key in pageData[item.link]) {
-      console.log("AAAAAAAAAAAAAAAAA0, ", pageData[item.link][key].link);
       acc[`/${item.link}/${pageData[item.link][key].link}`] = {
         static: staticData,
         sidebarName: item.name,
